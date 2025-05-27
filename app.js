ff756fd7c9e16763041d99f4bd2529295c86b3b1
AikKiat <silentak117@gmail.com>
2025-05-27 19:27:32 +0800

    updated projects page and hero header

--- app.js
+++ app.js
@@ -10,28 +10,6 @@ function createHeroDescriptions(autotypeTextUl,dynamicTexts){
     }
 }
 
-
-//typewriter function
-function typewriter(introText, text, textCaret, typingSpeed, introString, step){
-    if(step >= text.length)
-    {
-        const textCaretElement = introText.querySelector('#caret');
-        controlTextCaret(textCaretElement, 1000);
-        return true;
-    }
-
-    if(step < text.length)
-    {
-        introString += text.charAt(step);
-        introText.innerHTML = ""+introString + ""+textCaret;
-        step++;
-    }
-
-    setTimeout(() => {
-        typewriter(introText, text, textCaret, typingSpeed, introString, step);
-    }, typingSpeed);
-}
-
 function controlTextCaret(textCaret, timeout){
     textCaret.style.animationPlayState = "running";
     setTimeout(() => {
@@ -44,10 +22,38 @@ function controlTextCaret(textCaret, timeout){
 
 
 
+var bhasExit = false;
+
+//typewriter function
+function typewriter(introText, text, textCaret, typingSpeed, introString, step){
+    if(step >= text.length)
+    {
+        const textCaretElement = introText.querySelector('#caret');
+        controlTextCaret(textCaretElement, 1000);
+        return true;
+    }
+
+    if(step < text.length)
+    {
+        if (bhasExit == true){
+            return;
+        }
+        introString += text.charAt(step);
+        introText.innerHTML = ""+introString + ""+textCaret;
+        step++;
+    }
+
+    setTimeout(() => {
+        typewriter(introText, text, textCaret, typingSpeed, introString, step);
+    }, typingSpeed);
+}
+
+
+
 
 
 //add dynamic autotyping text function
-dynamicTexts = ["I am Aik Kiat, SUTD CSD", "Software Engineering", "Frontend Developer", "AI-Applications Enthusiast"];
+dynamicTexts = ["I am Aik Kiat, SUTD CSD", "Software Engineering", "Frontend Dev, Aspiring Fullstack", "AI-Applications Enthusiast"];
 const autotypeTextUl = document.querySelector(".dynamic__texts");
 const heroTextCaret = "<span class='hero__caret' id='caret'></span>"; //caretElement
 
@@ -75,7 +81,7 @@ function displayDynamicTexts(textSpanCount){
 }
 
 var textSpanCount = 0;
-var timeBetweenTexts = 1500;
+var timeBetweenTexts = 1000;
 
 displayDynamicTexts(textSpanCount);
 console.log(finishedDisplaying);
@@ -83,7 +89,7 @@ console.log(finishedDisplaying);
 setTimeout(() => {
     startButton.style.opacity = 1;
     startButton.style.zIndex = 1;
-}, 6000);
+}, 3000);
 
 
 function initialiseIntersectionObserver(){
@@ -303,3 +309,133 @@ function downloadResume(){
     location.href = fileLink;
 }
 
+
+
+smartHealth_text = `Android App created as Part of 50.001 Informations Systems and Programming Course.\n 
+Created using Java in Android Studio, and used Java Spring as the backend framework. 
+User data stored in a PostGreSQL database, hosted on AWS.\n 
+REST APIs for standardised clear communication standard between the client and backend, 
+and RESTful APIs include Google Places, Routes API, Open AI GPT4.0 Api.`
+
+
+popKids_text = `A virtual life-size simulation of fishes that morph and swirl according to users' hand gestures.
+Used OpenCV and Mediapipe for motion gesture detection, captured through an XBOX Kinect mounted on the ceiling. Done as part of our Term 2 Course in Design Thinking and Innovation.
+Spearheaded parsing of the AI prediction data as results to drive the fish particle elements in the simulation, and also set up the scene in Blender 3D to host this simulation, using the built-in Blender Python Library.`
+
+
+nlpNews_Text =`My own personal project on exploring the applications of NLP, namely on finding the relations between news articles. I used the Babelscape's REBEL Model, which uses a Transformer-based approach to extract 
+key text information from a given corpus, in the form of Triplets. This is at odds with conventional Classification Pipelines 
+which are two step in nature, firstly requiring a transformer to find all possible combinations of keywords, and then parsing the results to a trained classifier to determine the relations. Read more on my github repo!`
+
+
+mathMassacre_text=`Simple and fun game created during our first term at SUTD, using Python Standard Libraries and Tkinter. Project done as part of our Computational Thinking for Design course, as an introduction to simple programming.`
+
+
+fred_text=`My personal project for exploring how Real-time object detection can be implemented as a handheld feature, and thus I devled into Flutter App development as well. Used the TensorflowLite plugin as well as OpenCV for object detection. More info about it on my github repo.`
+
+
+greenLoop_text=`Team Project as part of SUTD's annual What-The-Hack(WTH) competition featuring sponsors such as DBS, HTX, Govtech and IMDA. As a team, we built a system that could sort recyclables automatically via OpenCV for Computer Vision, 
+and detecting the type of recyclable using an existing model. The prediction results were then passed over to an onboard ESP32 via Serial Comm, to drive the hardware components.`
+
+
+videoLinks = {"smartHealth":"https://www.youtube.com/embed/EaPBjWaO0MM?si=8EUNXFvua8doaU5p", "popKids":"https://www.youtube.com/embed/JwkDctVayhs?si=-H7w17iXeZvOiQ4x", 
+    "fred": "https://www.youtube.com/embed/oVhxidlmAhY?si=fD7OIAYSqiQlSckN", "mathMassacre":"https://www.youtube.com/embed/adQkJX5DadU?si=-48adIf1l1DBiVgI",
+    "nlpNews": "https://www.youtube.com/embed/5yQdkhj2GSM?si=E2srlXCG-6ffiaan", "greenLoop":"images/project_images/hacking2.jpg"}
+
+gitHubLinks = {"smartHealth":"https://github.com/ItsMeOX/SmartHealth", "popKids":"https://github.com/AikKiat/Pools-of-Possibilities-FIsh-Flock-Simulation-and-Motion-Gesture-Detection-in-Blender", 
+    "fred": "https://github.com/AikKiat/ObjectDetect-Test", "mathMassacre":"https://github.com/AikKiat/The-Mathematician-s-Massacre",
+    "nlpNews": "https://github.com/AikKiat/News-Article-Detective-NLP-Texts-Relations-Project-", "greenLoop":"https://github.com/AikKiat/-WTH2024-GreenLoop-Smart-Recyclables-Sorting"}
+
+
+function typeOutInfo(text, component){
+    let typingSpeed =10;
+    typewriter(component, text, heroTextCaret, typingSpeed,"\n \n ",0)
+}
+
+
+
+//Control the Projects Popup window
+
+function summonPopup(selector){
+    let animationTime = 50;
+    //get the popup window
+    popUpWindow = document.querySelector(".projects__popupWindow");
+    informationBody = document.querySelector(".projects__popupWindow .information__body");
+    videoSource = document.getElementById("project__information__video");
+    projectInfo = document.querySelector(".project__information");
+
+    holder = document.querySelector(".holder");
+
+
+    document.querySelector(".projects__popupWindow").animate(
+        [
+            {transform: 'skew(180deg)'}
+        ],
+        {
+            duration: animationTime,
+            iterations:'1'
+        }
+    )
+
+    if (selector == 'exit'){
+        setTimeout(() => {
+            videoSource.style.zIndex = 1;
+            videoSource.style.opacity = 1;
+            popUpWindow.style.opacity = 0;
+            popUpWindow.style.zIndex = -1;
+            informationBody.innerHTML = "";
+
+            bhasExit = true;
+        }, animationTime);
+        return;
+    }
+
+    popUpWindow.style.opacity = 1;
+    popUpWindow.style.zIndex = 2;
+    bhasExit = false;
+    
+    if (selector == 'popKids'){
+        typeOutInfo(popKids_text, informationBody);
+        videoSource.setAttribute('src', videoLinks['popKids']);
+        holder.setAttribute('href', gitHubLinks['popKids']);
+        console.log("popKids");
+    }
+
+    if (selector == 'smartHealth'){
+        typeOutInfo(smartHealth_text, informationBody);
+        videoSource.setAttribute('src', videoLinks['smartHealth']);
+        holder.href= gitHubLinks['smartHealth'];
+        console.log("smarthealth");
+    }
+
+    if (selector == 'fred'){
+        typeOutInfo(fred_text, informationBody);
+        videoSource.setAttribute('src', videoLinks['fred']);
+        holder.href= gitHubLinks['fred'];
+        console.log("fred");
+    }
+
+    if (selector == 'nlpNews'){
+        typeOutInfo(nlpNews_Text, informationBody);
+        videoSource.setAttribute('src', videoLinks['nlpNews']);
+        holder.href= gitHubLinks['nlpNews'];
+        console.log("nlpNews");
+    }
+
+    if (selector == 'mathMassacre'){
+        typeOutInfo(mathMassacre_text, informationBody);
+        videoSource.setAttribute('src', videoLinks['mathMassacre']);
+        holder.href=gitHubLinks['mathMassacre'];
+        console.log("mathMassacre");
+    }
+
+    if (selector == 'greenLoop'){
+        typeOutInfo(greenLoop_text, informationBody);
+        videoSource.style.zIndex = -2;
+        videoSource.style.opacity = 0;
+
+        holder.href= gitHubLinks['greenLoop'];
+        console.log("hacakthon");
+    }
+
+}
