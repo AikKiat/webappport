c978607644e2b3697cf7ab22c7a32c414968d8fb
Aik Kiat <millerman734@gmail.com>
2026-01-12 13:34:49 +0800

    updated video links

--- app.js
+++ app.js
@@ -373,7 +373,7 @@ detecting the type of recyclable using an existing model. The prediction results
 into the ESP32 to drive the hardware components in different ways to sort the different types of recyclables.`
 
 
-videoLinks = {"smartHealth":"https://www.youtube.com/embed/EaPBjWaO0MM?si=8EUNXFvua8doaU5p", "popKids":"https://www.youtube.com/embed/JwkDctVayhs?si=-H7w17iXeZvOiQ4x", 
+videoLinks = {"smartHealth":"https://www.youtube.com/embed/L9Ba1HxGkM4?si=xgpkq_-eoTMfbB5q", "popKids":"https://www.youtube.com/embed/BSN1rSyl0B4?si=Q2FH8AzupTnvPD36", 
     "fred": "https://www.youtube.com/embed/oVhxidlmAhY?si=fD7OIAYSqiQlSckN", "mathMassacre":"https://www.youtube.com/embed/adQkJX5DadU?si=-48adIf1l1DBiVgI",
     "nlpNews": "https://www.youtube.com/embed/5yQdkhj2GSM?si=E2srlXCG-6ffiaan", "greenLoop":"images/project_images/hacking2.jpg", "placeholder":"#"}
 
